03c273df9591e2601b3fe88defcc991fc688d554
Robert <rhorrace95@gmail.com>
2022-06-23 21:54:49 -0600

    added Bitonic, Comb, Gnome, and Shell sort Refactored time_tick usage and __sort method to use match for readability

--- visualizer.py
+++ visualizer.py
@@ -8,10 +8,12 @@ from colors import *
 class Visualizer:
     def __init__(self):
         self.__window = Tk()
-        self.__algorithms = ['Bubble Sort', 'Heap Sort', 'Insertion Sort', 'Merge Sort', 'Quick Sort', 'Selection Sort']
+        self.__algorithms = ['Bitonic Sort', 'Bubble Sort', 'Comb Sort', 'Gnome Sort', 'Heap Sort',
+                             'Insertion Sort', 'Merge Sort', 'Quick Sort', 'Selection Sort', 'Shell Sort']
         self.__speeds = ['Fast', 'Medium', 'Slow']
         self.__data = []
         self.__size = 0
+        self.__tick = 0.0
 
         self.__UI = Frame(self.__window, width=900, height=300, bg=WHITE)
         self.__UI.grid(row=0, column=0, padx=10, pady=5)
@@ -62,7 +64,7 @@ class Visualizer:
         self.__window.update_idletasks()
 
     def __generate(self):
-        self.__data = random.sample(range(1, 151), 150)
+        self.__data = random.sample(range(1, 129), 128)
         self.__size = len(self.__data)
 
         self.__draw_data([BLUE] * len(self.__data))
@@ -76,30 +78,62 @@ class Visualizer:
             return 0.01
 
     def __sort(self):
-        time_tick = self.__set_speed()
+        self.__time_tick = self.__set_speed()
 
-        if self.__drpAlgorithm.get() == 'Bubble Sort':
-            self.__bubble_sort(time_tick)
-
-        elif self.__drpAlgorithm.get() == 'Heap Sort':
-            self.__heap_sort(time_tick)
-
-        elif self.__drpAlgorithm.get() == 'Insertion Sort':
-            self.__insertion_sort(time_tick)
-
-        elif self.__drpAlgorithm.get() == 'Merge Sort':
-            self.__merge_sort(0, self.__size - 1, time_tick)
-
-        elif self.__drpAlgorithm.get() == 'Quick Sort':
-            self.__quick_sort(0, self.__size - 1, time_tick)
-
-        elif self.__drpAlgorithm.get() == 'Selection Sort':
-            self.__selection_sort(time_tick)
+        match self.__drpAlgorithm.get():
+            case 'Bitonic Sort':
+                self.__bitonic_sort(0, self.__size, 1)
+            case 'Bubble Sort':
+                self.__bubble_sort()
+            case 'Comb Sort':
+                self.__comb_sort()
+            case 'Gnome Sort':
+                self.__gnome_sort()
+            case 'Heap Sort':
+                self.__heap_sort()
+            case 'Insertion Sort':
+                self.__insertion_sort()
+            case 'Merge Sort':
+                self.__merge_sort(0, self.__size - 1)
+            case 'Quick Sort':
+                self.__quick_sort(0, self.__size - 1)
+            case 'Selection Sort':
+                self.__selection_sort()
+            case 'Shell Sort':
+                self.__shell_sort()
 
     # Sorts
 
+    # Bitonic Sort
+    def __bitonic_sort(self, low, cnt, dire):
+        if cnt > 1:
+            k = cnt // 2
+
+            self.__bitonic_sort(low, k, 1)
+            self.__bitonic_sort(low + k, k, 0)
+            self.__bitonic_merge(low, cnt, dire)
+
+        self.__draw_data([BLUE] * self.__size)
+
+    def __bitonic_merge(self, low, cnt, dire):
+        if cnt > 1:
+            k = cnt // 2
+
+            for i in range(low, low + k):
+                self.__comp_and_swap(i, i + k, dire)
+
+            self.__bitonic_merge(low, k, dire)
+            self.__bitonic_merge(low + k, k, dire)
+
+    def __comp_and_swap(self, i, j, dire):
+        if (dire == 1 and self.__data[i] > self.__data[j]) or (dire == 0 and self.__data[i] < self.__data[j]):
+            self.__draw_data([YELLOW if x == i or x == j else BLUE for x in range(self.__size)])
+            time.sleep(self.__time_tick)
+
+            self.__data[i], self.__data[j] = self.__data[j], self.__data[i]
+
     # Bubble Sort
-    def __bubble_sort(self, time_tick):
+    def __bubble_sort(self):
         swapped = False
         for n in range(self.__size - 1, 0, -1):
             for i in range(n):
@@ -109,28 +143,71 @@ class Visualizer:
                 swapped = True
                 self.__data[i], self.__data[i + 1] = self.__data[i + 1], self.__data[i]
                 self.__draw_data([YELLOW if x == i or x == i + 1 else BLUE for x in range(self.__size)])
-                time.sleep(time_tick)
+                time.sleep(self.__time_tick)
 
             if not swapped:
                 break
 
         self.__draw_data([BLUE] * self.__size)
 
-    # Heap Sort
-    def __heap_sort(self, time_tick):
-        for i in range(self.__size // 2 - 1, -1, -1):
-            self.__heapify(self.__size, i, time_tick)
+    # Comb Sort
+    def __comb_sort(self):
+        gap = self.__size
+        swapped = True
 
-        for i in range(self.__size - 1, 0, -1):
-            self.__draw_data([YELLOW if x == i else BLUE for x in range(self.__size)])
-            time.sleep(time_tick)
+        while gap != 1 or swapped:
+            gap = self.__get_next_gap(gap)
+            swapped = False
 
-            self.__data[i], self.__data[0] = self.__data[0], self.__data[i]
-            self.__heapify(i, 0, time_tick)
+            for i in range(0, self.__size - gap):
+                if self.__data[i] > self.__data[i + gap]:
+                    self.__draw_data([YELLOW if x == i or x == i + gap else BLUE for x in range(self.__size)])
+                    time.sleep(self.__time_tick)
+
+                    self.__data[i], self.__data[i + gap] = self.__data[i + gap], self.__data[i]
+                    swapped = True
 
         self.__draw_data([BLUE] * self.__size)
 
-    def __heapify(self, n, i, time_tick):
+    def __get_next_gap(self, gap):
+        gap = (gap * 10) // 13
+
+        return 1 if gap < 1 else gap
+
+    # Gnome Sort
+    def __gnome_sort(self):
+        index = 0
+
+        while index < self.__size:
+            if index == 0:
+                index += 1
+
+            if self.__data[index] >= self.__data[index - 1]:
+                index += 1
+            else:
+                self.__draw_data([YELLOW if x == index else BLUE for x in range(self.__size)])
+                time.sleep(self.__time_tick)
+
+                self.__data[index], self.__data[index - 1] = self.__data[index - 1], self.__data[index]
+                index -= 1
+
+        self.__draw_data([BLUE] * self.__size)
+
+    # Heap Sort
+    def __heap_sort(self):
+        for i in range(self.__size // 2 - 1, -1, -1):
+            self.__heapify(self.__size, i)
+
+        for i in range(self.__size - 1, 0, -1):
+            self.__draw_data([YELLOW if x == i else BLUE for x in range(self.__size)])
+            time.sleep(self.__time_tick)
+
+            self.__data[i], self.__data[0] = self.__data[0], self.__data[i]
+            self.__heapify(i, 0)
+
+        self.__draw_data([BLUE] * self.__size)
+
+    def __heapify(self, n, i):
         largest = i
         lt = 2 * i + 1
         rt = 2 * i + 2
@@ -143,14 +220,14 @@ class Visualizer:
 
         if largest != i:
             self.__draw_data([YELLOW if x == largest else BLUE for x in range(self.__size)])
-            time.sleep(time_tick)
+            time.sleep(self.__time_tick)
 
             self.__data[i], self.__data[largest] = self.__data[largest], self.__data[i]
 
-            self.__heapify(n, largest, time_tick)
+            self.__heapify(n, largest)
 
     # Insertion Sort
-    def __insertion_sort(self, time_tick):
+    def __insertion_sort(self):
         for i in range(1, self.__size):
             key = self.__data[i]
             j = i - 1
@@ -161,26 +238,26 @@ class Visualizer:
                 j -= 1
 
             self.__draw_data([YELLOW if x == j + 1 else BLUE for x in range(self.__size)])
-            time.sleep(time_tick)
+            time.sleep(self.__time_tick)
 
             self.__data[j + 1] = key
 
             self.__draw_data([BLUE] * self.__size)
 
     # Merge Sort
-    def __merge_sort(self, start, end, time_tick):
+    def __merge_sort(self, start, end):
         if start < end:
             mid = start + (end - start) // 2
-            self.__merge_sort(start, mid, time_tick)
-            self.__merge_sort(mid + 1, end, time_tick)
+            self.__merge_sort(start, mid)
+            self.__merge_sort(mid + 1, end)
 
-            self.__merge(start, mid, end, time_tick)
+            self.__merge(start, mid, end)
 
-            time.sleep(time_tick)
+            time.sleep(self.__time_tick)
 
         self.__draw_data([BLUE] * self.__size)
 
-    def __merge(self, start, mid, end, time_tick):
+    def __merge(self, start, mid, end):
         arr1 = self.__data[start:mid + 1]
         arr2 = self.__data[mid + 1:end + 1]
 
@@ -189,7 +266,7 @@ class Visualizer:
 
         while i < n1 and j < n2:
             self.__draw_data([YELLOW if x == k else BLUE for x in range(self.__size)])
-            time.sleep(time_tick)
+            time.sleep(self.__time_tick)
 
             if arr1[i] < arr2[j]:
                 self.__data[k] = arr1[i]
@@ -202,7 +279,7 @@ class Visualizer:
 
         while i < n1:
             self.__draw_data([YELLOW if x == k else BLUE for x in range(self.__size)])
-            time.sleep(time_tick)
+            time.sleep(self.__time_tick)
 
             self.__data[k] = arr1[i]
             i += 1
@@ -210,23 +287,23 @@ class Visualizer:
 
         while j < n2:
             self.__draw_data([YELLOW if x == k else BLUE for x in range(self.__size)])
-            time.sleep(time_tick)
+            time.sleep(self.__time_tick)
 
             self.__data[k] = arr2[j]
             j += 1
             k += 1
 
     # Quick Sort
-    def __quick_sort(self, low, high, time_tick):
+    def __quick_sort(self, low, high):
         if low < high:
-            pi = self.__partition(low, high, time_tick)
+            pi = self.__partition(low, high)
 
-            self.__quick_sort(low, pi - 1, time_tick)
-            self.__quick_sort(pi + 1, high, time_tick)
+            self.__quick_sort(low, pi - 1)
+            self.__quick_sort(pi + 1, high)
 
         self.__draw_data([BLUE] * self.__size)
 
-    def __partition(self, low, high, time_tick):
+    def __partition(self, low, high):
         pivot = self.__data[high]
 
         i = low - 1
@@ -234,27 +311,26 @@ class Visualizer:
         for j in range(low, high):
             if self.__data[j] <= pivot:
                 self.__draw_data([YELLOW if x == j else BLUE for x in range(self.__size)])
-                time.sleep(time_tick)
+                time.sleep(self.__time_tick)
 
                 i += 1
 
                 self.__data[i], self.__data[j] = self.__data[j], self.__data[i]
 
         self.__draw_data([YELLOW if x == i + 1 else BLUE for x in range(self.__size)])
-        time.sleep(time_tick)
+        time.sleep(self.__time_tick)
 
         self.__data[i + 1], self.__data[high] = self.__data[high], self.__data[i + 1]
 
         return i + 1
 
     # Selection Sort
-    def __selection_sort(self, time_tick):
-        size = len(self.__data)
-
-        for i in range(size):
+    def __selection_sort(self):
+        for i in range(self.__size):
             min_idx = i
-            for j in range(i + 1, size):
-                self.__draw_data([YELLOW if x == j else BLUE for x in range(size)])
+            for j in range(i + 1, self.__size):
+                self.__draw_data([YELLOW if x == j else BLUE for x in range(self.__size)])
+                time.sleep(self.__time_tick)
 
                 if self.__data[min_idx] <= self.__data[j]:
                     continue
@@ -263,4 +339,30 @@ class Visualizer:
 
             self.__data[i], self.__data[min_idx] = self.__data[min_idx], self.__data[i]
 
-        self.__draw_data([BLUE] * size)
+        self.__draw_data([BLUE] * self.__size)
+
+    # Shell Sort
+    def __shell_sort(self):
+        gap = self.__size // 2
+
+        while gap > 0:
+            j = gap
+
+            while j < self.__size:
+                i = j - gap
+
+                while i >= 0:
+                    if self.__data[i + gap] > self.__data[i]:
+                        break
+
+                    self.__draw_data([YELLOW if x == i or x == i + gap else BLUE for x in range(self.__size)])
+                    time.sleep(self.__time_tick)
+
+                    self.__data[i + gap], self.__data[i] = self.__data[i], self.__data[i + gap]
+                    i -= gap
+
+                j += 1
+
+            gap //= 2
+
+        self.__draw_data([BLUE] * self.__size)
